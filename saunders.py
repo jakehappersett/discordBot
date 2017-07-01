cceff5788abbdf97e841bfcef47b08b6d2bc5ec5
jake.happersett <jake.happersett@gmail.com>
2017-07-01 08:49:10 +0000

    commented old code

--- saunders.py
+++ saunders.py
@@ -15,32 +15,31 @@ class SaundersBot(ChatBot):
     client = discord.Client() 
     
 
-    
 
-
-client = discord.Client()
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
-
-@client.event
-async def on_message(message):
-    if message.content.startswith('.test'):
-        counter = 0
-        tmp = await client.send_message(message.channel, 'Calculating messages...')
-        async for log in client.logs_from(message.channel, limit=100):
-            if log.author == message.author:
-                counter += 1
-
-        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-    elif message.content.startswith('.sleep'):
-        await asyncio.sleep(5)
-        await client.send_message(message.channel, 'Done sleeping')
-    elif message.content.startswith('.ping'):
-        await client.send_message(message.channel, "Pong!")
-    elif message.content.startswith('.fucknat'):
-        await client.send_message(message.channel, "Fuck you, Nat")
-client.run('MzI5MzQwOTU1MzU2NTYxNDE5.DDRCSQ.P5dv1mvzh1Lz_uvL47F7ng9nG4c')
+#
+#client = discord.Client()
+#@client.event
+#async def on_ready():
+#    print('Logged in as')
+#    print(client.user.name)
+#    print(client.user.id)
+#    print('------')
+#
+#@client.event
+#async def on_message(message):
+#    if message.content.startswith('.test'):
+#        counter = 0
+#        tmp = await client.send_message(message.channel, 'Calculating messages...')
+#        async for log in client.logs_from(message.channel, limit=100):
+#            if log.author == message.author:
+#                counter += 1
+#
+#        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+#    elif message.content.startswith('.sleep'):
+#        await asyncio.sleep(5)
+#        await client.send_message(message.channel, 'Done sleeping')
+#    elif message.content.startswith('.ping'):
+#        await client.send_message(message.channel, "Pong!")
+#    elif message.content.startswith('.fucknat'):
+#        await client.send_message(message.channel, "Fuck you, Nat")
+#client.run('MzI5MzQwOTU1MzU2NTYxNDE5.DDRCSQ.P5dv1mvzh1Lz_uvL47F7ng9nG4c')
